49f6e05ed2a85d1f2997aeeb40307c3b986de433
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-02 01:25:02 +0000

    changes

--- app/src/main/java/uk/ac/aston/fyp/FileFragment.java
+++ app/src/main/java/uk/ac/aston/fyp/FileFragment.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-//FILE FRAGMENT - FRAGMENT DISPLAYS FILE 
+//FILE FRAGMENT - FRAGMENT DISPLAYS FILE
 
 /**
  * A simple {@link Fragment} subclass.
