eb962b29bfb037e82e5231423844029bf0e61ff7
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-02 13:29:27 +0000

    simplified app

--- app/src/main/java/uk/ac/aston/fyp/HomepageActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/HomepageActivity.java
@@ -23,11 +23,22 @@ public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        ActivityCompat.requestPermissions(this,
+                new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
     }
 
     public void addFile(View view) {
         Intent i = new Intent(HomepageActivity.this, TexteditorActivity.class);
         startActivity(i);
+
+        Log.i("create new", "creating new file");
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
+            intent.setType("*/*");
+            this.startActivity(intent);
+        }
     }
 
 }
--- app/src/main/java/uk/ac/aston/fyp/TexteditorActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/TexteditorActivity.java
@@ -19,19 +19,5 @@ public class TexteditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_texteditor);
-
-        ActivityCompat.requestPermissions(this,
-                new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
-    }
-
-    public void createNewFile(View view) {
-
-        Log.i("create new", "creating new file");
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-            Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
-            intent.setType("*/*");
-            this.startActivity(intent);
-        }
     }
 }
