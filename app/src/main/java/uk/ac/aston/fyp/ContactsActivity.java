56978741c0718603b40d2a8907ee32dc4b41558d
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-06 15:27:35 +0000

    additions

--- app/src/main/java/uk/ac/aston/fyp/ContactsActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/ContactsActivity.java
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
 
 public class ContactsActivity extends AppCompatActivity {
+
+    private EditText addContactText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+        addContactText = findViewById(R.id.addcontacttext);
     }
 
     public void toHome(View view) {
         Intent i = new Intent(ContactsActivity.this, HomepageActivity.class);
         startActivity(i);
+    }
+
+    public void addContact(View view) {
+        Log.i("contact add", addContactText.getText().toString());
     }
 }
