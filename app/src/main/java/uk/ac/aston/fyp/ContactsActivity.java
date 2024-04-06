3b85f294a402a9aa2fa31ff13aa49684f4e50c2e
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-06 15:11:46 +0000

    additions

--- app/src/main/java/uk/ac/aston/fyp/ContactsActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/ContactsActivity.java
@@ -2,7 +2,9 @@ package uk.ac.aston.fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ContactsActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ContactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+    }
+
+    public void toHome(View view) {
+        Intent i = new Intent(ContactsActivity.this, HomepageActivity.class);
+        startActivity(i);
     }
 }
