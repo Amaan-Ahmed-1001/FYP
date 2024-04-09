f1b84fc7a1e33eede9f4d11c5179f24419dfa7dc
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-09 13:43:14 +0000

    additions

--- app/src/main/java/uk/ac/aston/fyp/ContactsActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/ContactsActivity.java
@@ -7,7 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,10 +19,15 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+
 
 public class ContactsActivity extends AppCompatActivity {
 
     private EditText addContactText;
+    private ListView contactList;
+    private ArrayList<String> usersList;
+    private ArrayAdapter<String> usersListAdapter;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
@@ -28,6 +35,10 @@ public class ContactsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
         addContactText = findViewById(R.id.addcontacttext);
+        contactList = findViewById(R.id.contactslist);
+        usersList = new ArrayList<String>();
+        usersListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, usersList);
+        contactList.setAdapter(usersListAdapter);
     }
 
     public void toHome(View view) {
@@ -53,6 +64,7 @@ public class ContactsActivity extends AppCompatActivity {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.i("TAG", document.getId() + " => " + document.getData());
                             }
+                            usersListAdapter.add(addContactText.getText().toString());
                         } else {
                             Log.i( "Error getting documents: ", task.getException().toString());
                         }
