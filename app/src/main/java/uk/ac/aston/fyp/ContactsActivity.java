71a62efc2149a4bd9ba8a999f649fefef52b6e21
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-09 16:31:26 +0000

    additions

--- app/src/main/java/uk/ac/aston/fyp/ContactsActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/ContactsActivity.java
@@ -12,7 +12,10 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -20,6 +23,9 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ContactsActivity extends AppCompatActivity {
@@ -28,6 +34,7 @@ public class ContactsActivity extends AppCompatActivity {
     private ListView contactList;
     private ArrayList<String> usersList;
     private ArrayAdapter<String> usersListAdapter;
+    private String myDocId;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
@@ -39,6 +46,7 @@ public class ContactsActivity extends AppCompatActivity {
         usersList = new ArrayList<String>();
         usersListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, usersList);
         contactList.setAdapter(usersListAdapter);
+        myDocId = "";
     }
 
     public void toHome(View view) {
@@ -47,8 +55,8 @@ public class ContactsActivity extends AppCompatActivity {
     }
 
     public void addContact(View view) {
-        Log.i("contact add", addContactText.getText().toString());
 
+        //first get, if user exists
         db.collection("Users")
                 .whereEqualTo("user", addContactText.getText().toString())
                 .get()
@@ -59,10 +67,53 @@ public class ContactsActivity extends AppCompatActivity {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.i("TAG", document.getId() + " => " + document.getData());
                             }
+                            getDocId();
                             usersListAdapter.add(addContactText.getText().toString());
                         } else {
-                            Log.i( "Error getting documents: ", task.getException().toString());
+                            Log.i("Error getting documents: ", task.getException().toString());
                         }
+                    }
+                });
+    }
+
+    public void getDocId() {
+        //second get, current user's firestore doc id
+        db.collection("Users")
+                .whereEqualTo("user", FirebaseAuth.getInstance().getCurrentUser().getEmail())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.i("MyUser", document.getId() + " => " + document.getData());
+                                myDocId = document.getId();
+                                addToDatabase(myDocId);
+                            }
+                            Log.i("MyID", myDocId);
+                        } else {
+                            Log.i("Error getting documents: ", task.getException().toString());
+                        }
+                    }
+                });
+    }
+
+    public void addToDatabase(String documentId) {
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("listExample", Arrays.asList().add(addContactText.getText().toString()));
+
+        db.collection("Users").document(documentId)
+                .set(docData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i("TAG", "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i( "Error writing document", e.toString());
                     }
                 });
     }
