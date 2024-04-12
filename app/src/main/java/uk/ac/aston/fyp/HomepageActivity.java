58bc7e159ceb5e01b12afea63fe5c4728c6c04ad
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-12 03:53:17 +0000

    additions

--- app/src/main/java/uk/ac/aston/fyp/HomepageActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/HomepageActivity.java
@@ -1,17 +1,31 @@
 package uk.ac.aston.fyp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.Manifest;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 
 //HOMEPAGE ACTIVITY - INCLUDES HOMEPAGE OPTIONS AND USER'S LATEST ADDED FILES
 
 public class HomepageActivity extends AppCompatActivity {
+
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +40,8 @@ public class HomepageActivity extends AppCompatActivity {
     public void toContacts(View view) {
         Intent i = new Intent(HomepageActivity.this, ContactsActivity.class);
         startActivity(i);
+    }
+
+    public void uploadFile(View view) {
     }
 }
