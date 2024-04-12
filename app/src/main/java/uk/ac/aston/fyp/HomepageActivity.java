281e99bee17e440da291b61ecab117123cf9dc66
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-12 14:30:03 +0000

    additions

--- app/src/main/java/uk/ac/aston/fyp/HomepageActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/HomepageActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.Manifest;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -30,9 +32,6 @@ import java.io.File;
 //HOMEPAGE ACTIVITY - INCLUDES HOMEPAGE OPTIONS AND USER'S LATEST ADDED FILES
 
 public class HomepageActivity extends AppCompatActivity {
-
-    StorageReference storageReference;
-    DatabaseReference databaseReference;
     Uri imageuri = null;
 
 
@@ -51,8 +50,17 @@ public class HomepageActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void createFile() {
-        
+    public void toSendFile(View view) {
+        Intent i = new Intent(HomepageActivity.this, SendFileActivity.class);
+        startActivity(i);
+    }
+
+    public void createFile(View view) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
+            intent.setType("*/*");
+            this.startActivity(intent);
+        }
     }
 
     public void uploadFile(View view) {
@@ -80,7 +88,7 @@ public class HomepageActivity extends AppCompatActivity {
             Toast.makeText(HomepageActivity.this, imageuri.toString(), Toast.LENGTH_SHORT).show();
 
             // Here we are uploading the pdf in firebase storage with the name of current time
-            final StorageReference filepath = storageReference.child(messagePushID + "." + "pdf");
+            final StorageReference filepath = storageReference.child(FirebaseAuth.getInstance().getCurrentUser().getEmail() + "/" + messagePushID + "." + "pdf");
             Toast.makeText(HomepageActivity.this, filepath.getName(), Toast.LENGTH_SHORT).show();
             filepath.putFile(imageuri).continueWithTask(new Continuation() {
                 @Override
@@ -97,8 +105,6 @@ public class HomepageActivity extends AppCompatActivity {
                         // After uploading is done it progress
                         // dialog box will be dismissed
                         Uri uri = task.getResult();
-                        String myurl;
-                        myurl = uri.toString();
                         Toast.makeText(HomepageActivity.this, "Uploaded Successfully", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(HomepageActivity.this, "UploadedFailed", Toast.LENGTH_SHORT).show();
