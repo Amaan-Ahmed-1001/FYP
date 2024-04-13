1ad3bd626ab45dec26cf05bd797abae0cb0d5610
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-13 23:38:41 +0000

    adding dark mode feature

--- app/src/main/java/uk/ac/aston/fyp/SettingsActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/SettingsActivity.java
@@ -1,14 +1,44 @@
 package uk.ac.aston.fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Button lightmodeButton;
+    Button darkmodeButton;
+    boolean nightmode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        lightmodeButton = findViewById(R.id.lightmodebutton);
+        darkmodeButton = findViewById(R.id.darkmodebutton);
+
+        sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE);
+        nightmode = sharedPreferences.getBoolean("night", false);
+    }
+
+    public void lightMode(View view) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        editor = sharedPreferences.edit();
+        editor.putBoolean("night", false);
+        editor.apply();
+    }
+
+    public void darkMode(View view) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        editor = sharedPreferences.edit();
+        editor.putBoolean("night", true);
+        editor.apply();
     }
 }
