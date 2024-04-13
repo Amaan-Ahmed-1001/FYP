4dd0e31793cead3a79a6269509f42bfe381769ae
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-13 16:12:11 +0000

    additions

--- app/src/main/java/uk/ac/aston/fyp/SendFileActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/SendFileActivity.java
@@ -1,14 +1,86 @@
 package uk.ac.aston.fyp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class SendFileActivity extends AppCompatActivity {
+
+    EditText chooseContact;
+    Uri imageuri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_file);
+        chooseContact = findViewById(R.id.choosecontact);
+    }
+
+    public void sendFile(View view) {
+        if (chooseContact.getText().toString().equals(null)) {
+            Toast.makeText(SendFileActivity.this, "emptytext", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent i = new Intent();
+            i.setAction(Intent.ACTION_GET_CONTENT);
+
+            // We will be redirected to choose pdf
+            i.setType("application/pdf");
+            startActivityForResult(i, 1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+
+            // this will show message uploading
+            // while pdf is uploading
+            imageuri = data.getData();
+            final String timestamp = "" + System.currentTimeMillis();
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+            final String messagePushID = timestamp;
+            Toast.makeText(SendFileActivity.this, imageuri.toString(), Toast.LENGTH_SHORT).show();
+
+            // Here we are uploading the pdf in firebase storage with the name of current time
+            final StorageReference filepath = storageReference.child(chooseContact.getText().toString() + "/" + messagePushID + "." + "pdf");
+            Toast.makeText(SendFileActivity.this, filepath.getName(), Toast.LENGTH_SHORT).show();
+            filepath.putFile(imageuri).continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return filepath.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        // After uploading is done it progress
+                        // dialog box will be dismissed
+                        Uri uri = task.getResult();
+                        Toast.makeText(SendFileActivity.this, "Uploaded Successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SendFileActivity.this, "UploadedFailed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
