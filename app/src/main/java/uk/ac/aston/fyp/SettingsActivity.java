628448bd44eaf92c20d791fe7cee8db69fa543fd
Amaan-Ahmed-1001 <93011450+Amaan-Ahmed-1001@users.noreply.github.com>
2024-04-14 03:27:36 +0000

    additions

--- app/src/main/java/uk/ac/aston/fyp/SettingsActivity.java
+++ app/src/main/java/uk/ac/aston/fyp/SettingsActivity.java
@@ -1,18 +1,27 @@
 package uk.ac.aston.fyp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingsActivity extends AppCompatActivity {
 
     Button lightmodeButton;
     Button darkmodeButton;
+    Button deleteAccountButton;
     boolean nightmode;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
@@ -23,6 +32,7 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         lightmodeButton = findViewById(R.id.lightmodebutton);
         darkmodeButton = findViewById(R.id.darkmodebutton);
+        deleteAccountButton = findViewById(R.id.deleteaccountbutton);
 
         sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE);
         nightmode = sharedPreferences.getBoolean("night", false);
@@ -40,5 +50,22 @@ public class SettingsActivity extends AppCompatActivity {
         editor = sharedPreferences.edit();
         editor.putBoolean("night", true);
         editor.apply();
+    }
+
+    public void deleteAccount(View view) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.i("deleting account", "User account deleted.");
+                        }
+                    }
+                });
+
+        Intent i = new Intent(SettingsActivity.this, MainActivity.class);
+        startActivity(i);
     }
 }
